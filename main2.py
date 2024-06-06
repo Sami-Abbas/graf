f1ceb894f85aac77f9db8d53a1e0915eb57dd36a
Sami-Abbas <sami_abbas09@outlook.com>
2024-06-06 16:12:14 +0100

    Update main2.py

--- main2.py
+++ main2.py
@@ -42,7 +42,7 @@ def create_data(timestamp_now, timestamp_prev):
         }
     ]
 
-# Define the headers and endpoint
+
 headers = {
     "Authorization": "Bearer 149418:eyJrIjoiZTVkYWY1N2MzZmNmYjIzMjU0YTNlNDhlMWFmZjlhZmNiMTAxNGJhNSIsIm4iOiJ0ZXN0IiwiaWQiOjQyMDQ5fQ==",
     "Content-Type": "application/json"
@@ -50,19 +50,19 @@ headers = {
 ingest_endpoint = "https://graphite-blocks-prod-us-central1.grafana.net/graphite/metrics"
 
 while True:
-    # Calculate the current and previous rounded timestamps
+   
     timestamp_now_rounded = get_rounded_timestamp()
     timestamp_prev_rounded = timestamp_now_rounded - 10
 
-    # Create the data to be sent
+
     data = create_data(timestamp_now_rounded, timestamp_prev_rounded)
 
-    # Make the POST request
+
     response = requests.post(ingest_endpoint, headers=headers, data=json.dumps(data))
 
-    # Print the response status and content
+
     print(response.status_code)
     print(response.content)
 
-    # Wait for 10 seconds before sending the next request.
+
     time.sleep(10)
